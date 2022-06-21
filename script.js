a6ba8cb6344ca30606590b9cb273a948acc606be
lenrich <lenrich1403@gmail.com>
2022-06-21 17:51:25 +0800

    add localstorage

--- script.js
+++ script.js
@@ -6,12 +6,17 @@ const movieSelect = document.getElementById('movie');
 let ticketPrice = +movieSelect.value;
 // console.log(ticketPrice)
 
+populateUI()
 // 更新座位數及總票價
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
     // console.log(selectedSeats);
 
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat))
 
+    // console.log(seatsIndex)
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
 
     const selectedSeatsCount = selectedSeats.length;
     // console.log(selectedSeatsCount);
@@ -19,10 +24,34 @@ function updateSelectedCount() {
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// 保存電影索引值和票價
+function setMovieData(movieIndex, moivePrice) {
+    localStorage.setItem('selectedMoiveIndex', movieIndex)
+    localStorage.setItem('selectedMoivePrice', moivePrice)
+}
+// 獲取localstorage資料並渲染
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'))
+    // console.log(selectedSeats)
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected')
+            }
+        })
+    }
+
+    const selectMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if (selectMovieIndex !== null) {
+        movieSelect.selectedIndex = selectMovieIndex;
+    }
+}
+
 // 電影下拉框事件監聽
 movieSelect.addEventListener('change', (e) => {
     ticketPrice = +e.target.value;
     // console.log(ticketPrice);
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
 
@@ -36,3 +65,6 @@ container.addEventListener('click', (e) => {
         updateSelectedCount();
     }
 })
+
+// 設定init座位和總票價
+updateSelectedCount();
